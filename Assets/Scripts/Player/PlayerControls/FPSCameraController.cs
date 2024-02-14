ffa94d0d66540a60c4c233335c9959fa70ca6421
JavierTai <93179647+JavierTai@users.noreply.github.com>
2024-02-14 09:52:15 +0800

    Added Missing Files

---
+++ Assets/Scripts/Player/PlayerControls/FPSCameraController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Camera Controls
+
+public class FPSCameraController : MonoBehaviour
+{
+    private float verticalRotation = 0f;
+    private float horizontalRotation = 0f;
+    public float mouseSensitivity = 2.0f;
+
+
+    //private GameObject player;
+    private bool debugLock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        debugLock = false;  //debugging tool, presses escape to lock the player's view
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleCameraRotation();
+        if (Input.GetKeyDown("escape"))
+        {
+            if(debugLock == false)
+            {
+                debugLock = true;
+            }
+            else
+            {
+                debugLock = false;
+            }
+            
+        }
+    }
+
+    private void HandleCameraRotation()
+    {
+        if(debugLock == false)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+            horizontalRotation += mouseX;
+
+            verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+            verticalRotation = Mathf.Clamp(verticalRotation, -90, 90);
+
+            transform.Rotate(0, mouseX, 0);
+            Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, horizontalRotation, 0);
+
+        }
+    }
+   
+}
---
+++ Assets/Scripts/Player/PlayerControls/FPSControls.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class FPSControls : Entity   //Main Controller for all player movements, also derives from entity
+{
+
+    private MovementControl movementControl;
+    private SprintControl sprintControl;
+    private JumpControl jumpControl;
+    private CrouchControl crouchControl;
+    private TimeTravelControl timetravelControl;
+    private GunController gunController;
+
+    [SerializeField]
+    private PerlinNoiseShake perlinNoiseShake;
+    [SerializeField]
+    private HeadbobShake headbobShake;
+    [SerializeField]
+    private GunRecoilShake gunRecoilShake;
+    [SerializeField]
+    private GunBobShake gunBobShake;
+    [SerializeField]
+    private PlayerStatsUIManager playerStatsUIManager;
+    [SerializeField]
+    private CameraRecoil cameraRecoil;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementControl = GetComponent<MovementControl>();
+        jumpControl = GetComponent<JumpControl>();
+        crouchControl = GetComponent<CrouchControl>();
+        gunController = GetComponent<GunController>();
+        sprintControl = GetComponent<SprintControl>();
+        playerStatsUIManager.UpdateHealthUI(health.ToString());
+
+        gunController.SubscribeShootResponse(playerStatsUIManager);
+        gunController.SubscribeShootResponse(cameraRecoil);
+
+        gunController.SubscribeReloadResponse(playerStatsUIManager.UpdateAmmoUI);
+        gunController.SubscribeReloadResponse(playerStatsUIManager.UpdateReloadTimerUI);
+
+        gunController.SubscribeSwapResponse(playerStatsUIManager.UpdateAmmoUI);
+        gunController.SubscribeGrenadeResponse(playerStatsUIManager.UpdateGrenadeUI);
+
+        sprintControl.Subscribe(gunController);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sprintControl.Sprint();
+        jumpControl.Jump();
+        crouchControl.ActivateCrouch();
+        gunController.HandleShooting();
+        movementControl.Movement();
+    }
+
+
+    public override void OnDamaged(float _damage)
+    {
+        health -= _damage;
+        playerStatsUIManager.UpdateHealthUI(health.ToString());
+    }
+
+}
