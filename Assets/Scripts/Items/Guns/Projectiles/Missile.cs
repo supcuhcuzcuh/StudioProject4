87202691c8963b49b34ba675d630cde33e0dee72
JavierTai <93179647+JavierTai@users.noreply.github.com>
2024-02-15 15:23:18 +0800

    Polished some weapons

--- Assets/Scripts/Items/Guns/Projectiles/Missile.cs
+++ Assets/Scripts/Items/Guns/Projectiles/Missile.cs
@@ -4,41 +4,54 @@ using UnityEngine;
 using UnityEngine.Pool;
 
  // projectile revised to use UnityEngine.Pool in Unity 2021
-    public class Missile : RevisedProjectile
+public class Missile : RevisedProjectile
+{
+
+    [SerializeField]
+    private ParticleEffectManager particleEffectManager;
+
+    public float radius = 10f;
+    public float force = 300f;
+
+    private Rigidbody rb;
+
+    void OnEnable()
     {
+        //particleEffectManager.PlayParticleEffect("BazookaRocketTrail", transform.position, transform);
+    }
 
-        [SerializeField]
-        private ParticleEffectManager particleEffectManager;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
-        public float radius = 10f;
-        public float force = 300f;
+    void Update()
+    {
+        // Rotate the forward vector towards the target direction by one step
+        transform.forward = Vector3.Slerp(transform.forward, -transform.up, (rb.velocity.x + rb.velocity.z) * 0.03f * Time.deltaTime);
+    }
 
-        void OnEnable()
+    void  OnCollisionEnter(Collision _col)
+    {                       
+        ProjectileCollide(_col.gameObject);
+
+        particleEffectManager.PlayParticleEffect("Explosion", transform.position);
+        GameObject.Find("CameraHolder").GetComponent<PerlinNoiseShake>().QueueExplosion(4, 1);
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider nearbyObject in colliders)
         {
-            //particleEffectManager.PlayParticleEffect("BazookaRocketTrail", transform.position, transform);
-        }
-
-        void  OnCollisionEnter(Collision _col)
-        {                       
-            ProjectileCollide(_col.gameObject);
-
-            particleEffectManager.PlayParticleEffect("Explosion", transform.position);
-            GameObject.Find("CameraHolder").GetComponent<PerlinNoiseShake>().QueueExplosion(7, 1);
-            Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
-            foreach (Collider nearbyObject in colliders)
+            if (nearbyObject.gameObject.GetComponent<DesctrutableObjects>())
             {
-                if (nearbyObject.gameObject.GetComponent<DesctrutableObjects>())
-                {
-                    nearbyObject.gameObject.GetComponent<DesctrutableObjects>().OnDamaged(weaponSource.weaponDamage);
-                }
-
-                Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
-                if (rb != null)
-                {
-                    rb.AddExplosionForce(force, transform.position, radius);
-                }
-
+                nearbyObject.gameObject.GetComponent<DesctrutableObjects>().OnDamaged(weaponSource.weaponDamage);
             }
+
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+
         }
     }
+}
 
--- Assets/Scripts/Player/PlayerControls/TimeTravelControl.cs
+++ Assets/Scripts/Player/PlayerControls/TimeTravelControl.cs
@@ -73,10 +73,32 @@ public class TimeTravelControl : MonoBehaviour
         {
             if (thumbMover2.weight < 1)
             {
-                thumbMover2.weight += 2.0f * Time.deltaTime;
+                thumbMover2.weight += 4.0f * Time.deltaTime;
             } 
             else
             {
+                if (inPresent)
+                {
+                    inPresent = false;
+                    Camera.main.cullingMask &= ~(1 << 10);  //Add past layermask into camera's culling mask (so that you can see all past objects)
+                    Camera.main.cullingMask |= (1 << 9);    //Remove present layermask into camera's culling mask (so that you cannot see all present objects)
+
+                    altTimelineCam.cullingMask &= ~(1 << 9);     //Do the opposite for the altTimeline camera
+                    altTimelineCam.cullingMask |= (1 << 10);
+                    NotifyOnTimeTravelResponse();
+                }
+                else
+                {
+                    inPresent = true;
+                    Camera.main.cullingMask &= ~(1 << 9);   //Add present layermask into camera's culling mask (so that you can see all present objects)
+                    Camera.main.cullingMask |= (1 << 10);   //Remove past layermask into camera's culling mask (so that you cannot see all past objects)
+
+                    altTimelineCam.cullingMask &= ~(1 << 10);   //Do the opposite for the altTimeline camera
+                    altTimelineCam.cullingMask |= (1 << 9);
+                    NotifyOffTimeTravelResponse();
+                }
+
+
                 thumbDownAnim = false;
                 armWayAnim = true;
             }
@@ -164,29 +186,7 @@ public class TimeTravelControl : MonoBehaviour
         {
             if (inCollision == false)
             {
-                thumbDownAnim = true;
-                if (inPresent)
-                {
-                    inPresent = false;
-                    Camera.main.cullingMask &= ~(1 << 10);  //Add past layermask into camera's culling mask (so that you can see all past objects)
-                    Camera.main.cullingMask |= (1 << 9);    //Remove present layermask into camera's culling mask (so that you cannot see all present objects)
-
-                    altTimelineCam.cullingMask &= ~(1 << 9);     //Do the opposite for the altTimeline camera
-                    altTimelineCam.cullingMask |= (1 << 10);
-
-                    NotifyOnTimeTravelResponse();
-                }
-                else
-                {
-                    inPresent = true;
-                    Camera.main.cullingMask &= ~(1 << 9);   //Add present layermask into camera's culling mask (so that you can see all present objects)
-                    Camera.main.cullingMask |= (1 << 10);   //Remove past layermask into camera's culling mask (so that you cannot see all past objects)
-
-                    altTimelineCam.cullingMask &= ~(1 << 10);   //Do the opposite for the altTimeline camera
-                    altTimelineCam.cullingMask |= (1 << 9);
-
-                    NotifyOffTimeTravelResponse();
-                }
+                thumbDownAnim = true;             
             }
             else
             {
